37216ef67594a88b472bf3b0371551f59b3b6271
Slz <felipegoncalves2311@gmail.com>
2023-06-07 21:02:27 -0300

    Rework do Explorador de arquivos(#1) - Ícones para todas as pastas(até as sem ícone);

--- src/main/kotlin/IDE/ExploradorDeArquivos.kt
+++ src/main/kotlin/IDE/ExploradorDeArquivos.kt
@@ -1,16 +1,15 @@
 package IDE
 
+import java.awt.Component
 import java.awt.Dimension
-import java.awt.PopupMenu
-import java.awt.event.MouseEvent
-import java.awt.event.MouseListener
 import java.io.File
 import javax.swing.JPanel
 import javax.swing.JScrollPane
 import javax.swing.JTree
-import javax.swing.SwingUtilities
+import javax.swing.UIManager
 import javax.swing.event.TreeSelectionListener
 import javax.swing.tree.DefaultMutableTreeNode
+import javax.swing.tree.DefaultTreeCellRenderer
 
 class ExploradorDeArquivos(caminhoInicial: String, dimensao: Dimension) : JPanel() {
 
@@ -20,90 +19,89 @@ class ExploradorDeArquivos(caminhoInicial: String, dimensao: Dimension) : JPanel
     init {
         this.preferredSize = dimensao
         this.minimumSize = dimensao
-        this.isOpaque = true
         this.isVisible = true
-        raiz = carregarRaiz(File(caminhoInicial))
+        this.isOpaque = true
+
+        raiz = arquivosDaRaiz(File(caminhoInicial))
 
         arvore = JTree(raiz).apply {
+            cellRenderer = RenderizadorDeNodes()
             preferredSize = dimensao
             isVisible = true
-            addTreeSelectionListener { clickedComponent ->
-                val arquivo = File(clickedComponent.path.toString())
-                if (arquivo.isDirectory) {
-                    arquivosDaPasta(arquivo).forEach {
-
-                    }
-                }
-            }
+            isOpaque = true
         }
 
-        arvore.addHierarchyListener {}
-        arvore = JTree(carregarRaiz(File(caminhoInicial)))
-
-        // Código para adicionar a funcionalidade de menu de contexto no explorador.
-        val popupMenu = PopupMenu() // adicionar coisas a isso
-        addMouseListener(object : MouseListener {
-            override fun mouseClicked(e: MouseEvent?) {
-                e ?: throw NullPointerException()
-
-                if (SwingUtilities.isRightMouseButton(e)) {
-                    val row = arvore.getClosestRowForLocation (e.x, e.y);
-                    arvore.setSelectionRow(row);
-                    popupMenu.show(e.getComponent(), e.getX(), e.getY());
-                }
-            }
-            override fun mousePressed(e: MouseEvent?) {
-                TODO("Not yet implemented")
-            }
-            override fun mouseReleased(e: MouseEvent?) {
-                TODO("Not yet implemented")
-            }
-            override fun mouseEntered(e: MouseEvent?) {
-                TODO("Not yet implemented")
-            }
-            override fun mouseExited(e: MouseEvent?) {
-                TODO("Not yet implemented")
-            }
-        })
-        val scrollPane = JScrollPane().apply {
-            size = dimensao
-            add(arvore)
+        val scrollPane = JScrollPane(arvore).apply {
+            preferredSize = dimensao
+            isVisible = true
+            isOpaque = true
         }
-        add(scrollPane)
+
+        this.add(scrollPane)
+
+//        Código para adicionar a funcionalidade de menu de contexto no explorador.
+//        val popupMenu = PopupMenu() // adicionar coisas a isso
+//        addMouseListener(object : MouseListener {
+//            override fun mouseClicked(e: MouseEvent?) {
+//                e ?: throw NullPointerException()
+//
+//                if (SwingUtilities.isRightMouseButton(e)) {
+//                    val row = arvore.getClosestRowForLocation (e.x, e.y);
+//                    arvore.setSelectionRow(row);
+//                    popupMenu.show(e.getComponent(), e.getX(), e.getY());
+//                }
+//            }
+//            override fun mousePressed(e: MouseEvent?) {
+//                TODO("Not yet implemented")
+//            }
+//            override fun mouseReleased(e: MouseEvent?) {
+//                TODO("Not yet implemented")
+//            }
+//            override fun mouseEntered(e: MouseEvent?) {
+//                TODO("Not yet implemented")
+//            }
+//            override fun mouseExited(e: MouseEvent?) {
+//                TODO("Not yet implemented")
+//            }
+//        })
+
+
     }
 
     fun addTreeSelectionListener(listener: TreeSelectionListener) {
         arvore.addTreeSelectionListener(listener)
     }
 
-    /**
-     * Algoritimo Breadth First Search para pegar todas as pastas
-     * e retornar um DefaultMutableTreeNode contendo toda a
-     * hierarquia de pastas e arquivos
-     * @param raiz O diretório raiz do projeto.
-     * @return o DefaultMutableTreeNode contendo a hierarquia de pastas e arquivos.
-     * @author Slz
-     */
-    private fun carregarRaiz(raiz: File): DefaultMutableTreeNode {
-        if (!raiz.isDirectory) return DefaultMutableTreeNode(raiz.name)
-        val raizNode = DefaultMutableTreeNode(raiz.name, true).apply {
-            raiz.listFiles()?.forEach { file ->
-                if (file.isDirectory) add(DefaultMutableTreeNode(file.name, true))
-                else add(DefaultMutableTreeNode(file.name, false))
+    private fun arquivosDaRaiz(raiz: File): DefaultMutableTreeNode {
+        val raizNode = DefaultMutableTreeNode(raiz.name)
+        if (!raiz.isDirectory) return raizNode
+
+        raiz.listFiles()?.forEach { arquivo ->
+            if (arquivo.isFile) raizNode.add(DefaultMutableTreeNode(arquivo.name))
+        }
+        return DefaultMutableTreeNode()
+    }
+
+    class RenderizadorDeNodes: DefaultTreeCellRenderer() {
+
+        override fun getTreeCellRendererComponent(
+            tree: JTree?,
+            value: Any?,
+            sel: Boolean,
+            expanded: Boolean,
+            leaf: Boolean,
+            row: Int,
+            hasFocus: Boolean
+        ): Component {
+            super.getTreeCellRendererComponent(tree, value, sel, expanded, leaf, row, hasFocus)
+
+            if (value is DefaultMutableTreeNode && value.allowsChildren) {
+                icon = UIManager.getIcon("FileView.directoryIcon")
             }
+            return this
         }
-
-        return raizNode
     }
 
-    private fun arquivosDaPasta(pasta: File): List<DefaultMutableTreeNode> {
-        val listaDeArquivos = mutableListOf<DefaultMutableTreeNode>()
-        pasta.listFiles()?.forEach { file ->
-            if (file.isDirectory) listaDeArquivos.add(DefaultMutableTreeNode(file.name, true))
-            else listaDeArquivos.add(DefaultMutableTreeNode(file.name, false))
-        }
-        return listaDeArquivos
-    }
 }
 
 
