b479adc9d854577ab3ecfbb2275f3c8baba17750
Slz <felipegoncalves2311@gmail.com>
2023-06-08 15:39:40 -0300

    Wrapper para acessar a pasta de resources criado.

---
+++ src/main/kotlin/IDE/util/ResourcesManager.kt
@@ -0,0 +1,29 @@
+package IDE.util
+
+import javax.swing.ImageIcon
+
+/**
+ * Wrapper para facilitar o acesso à pasta de resources
+ * @author Slz
+ */
+object ResourcesManager {
+
+    /**
+     * Pega um recurso da pasta de recursos a partir do caminho dado.
+     * @param caminho O caminho do recurso a partir da pasta de resources.
+     * @return URL do recurso
+     * @author Slz
+     * @author Barril
+     * @see getIcon
+     */
+    fun getResource(caminho: String) = javaClass.classLoader.getResource(caminho)
+
+    /**
+     * Pega um ImageIcon de um dado caminho.
+     * @param caminho O caminho do ícone a partir da pasta de resources.
+     * @return o ImageIcon do caminho especificado.
+     * @author Slz
+     * @see getResource
+     */
+    fun getIcon(caminho: String) = ImageIcon(getResource(caminho))
+}
